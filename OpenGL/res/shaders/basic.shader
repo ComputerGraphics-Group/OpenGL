7acfe4c70092c85d7f588ef521e2e94bdb2eef7e
greenfish <pranavbalaji2@gmail.com>
2021-03-13 04:09:35 +0530

    Added gifs and colored fields

--- OpenGL/res/shaders/basic.shader
+++ OpenGL/res/shaders/basic.shader
@@ -2,13 +2,14 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
+layout(location = 1) in vec2 color;
 
-//out vec4 vertex_color;
+out vec4 vertex_color;
 
 void main()
 {
     gl_Position = position;
-//    vertex_color = position;
+    vertex_color = vec4(1.0, color.y, color.x, 0.0);
 };
 
 #shader fragment
@@ -16,9 +17,9 @@ void main()
 
 layout(location = 0) out vec4 color;
 
-//in vec4 vertex_color;
+in vec4 vertex_color;
 
 void main()
 {
-    color = vec4( 1, 1, 0, 1 );
+    color = vertex_color;
 };
--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <chrono>
 
 ///
 /// Struct to hold the vertex shader and fragment shader as strings
@@ -20,8 +21,14 @@ struct ShaderProgramSource
 ///
 struct Vec
 {
-    int i;
-    int j;
+    float i;
+    float j;
+};
+
+struct Color
+{
+    int x;
+    int y;
 };
 
 ///
@@ -105,73 +112,113 @@ static unsigned int createShader(const std::string& vertex_shader, const std::st
 ///
 /// Bresenham's line drawing algorithm
 ///
-void makeLine(std::vector<float> &points, int x1, int y1, int x2, int y2)
+Color makeLine(std::vector<float>& points, int x1, int y1, int x2, int y2, bool is_vector, Color c)
 {
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    int i, d;
-    int incNE, incE;
-    int x, y;
-    if (dx < 0)
-        dx = -dx;
-    if (dy < 0) 
-        dy = -dy;
-    int incx = 1;
-    if (x2 < x1)
-        incx = -1;
-    int incy = 1;
-    if (y2 < y1)
-        incy = -1;
-    x = x1; y = y1;
-    if (dx > dy)
-    {
-        points.push_back(x);
-        points.push_back(y);
-
-        d = 2 * dy - dx;
-        incNE = 2 * (dy - dx);
-        incE = 2 * dy;
-        for (i = 0; i < dx; i++)
-        {
-            if (d >= 0)
-            {
-                y += incy;
-                d += incNE;
-            }
-            else
-                d += incE;
-            x += incx;
-            points.push_back(x);
-            points.push_back(y);
-        }
-    }
-    else
-    {
-        points.push_back(x);
-        points.push_back(y);
-        d = 2 * dx - dy;
-        incNE = 2 * (dx - dy);
-        incE = 2 * dx;
-        for (i = 0; i < dy; i++)
-        {
-            if (d >= 0)
-            {
-                x += incx;
-                d += incNE;
-            }
-            else
-                d += incE;
-            y += incy;
-            points.push_back(x);
-            points.push_back(y);
-        }
-    }
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int i, d;
+	int incNE, incE;
+	int x, y;
+	if (dx < 0)
+		dx = -dx;
+	if (dy < 0)
+		dy = -dy;
+	int incx = 1;
+	if (x2 < x1)
+		incx = -1;
+	int incy = 1;
+	if (y2 < y1)
+		incy = -1;
+	x = x1; y = y1;
+	if (dx > dy)
+	{
+		points.push_back(x);
+		points.push_back(y);
+		if (is_vector)
+		{
+			points.push_back(dx);
+			points.push_back(dy);
+		}
+		else
+		{
+			points.push_back(c.x);
+			points.push_back(c.y);
+		}
+		d = 2 * dy - dx;
+		incNE = 2 * (dy - dx);
+		incE = 2 * dy;
+		for (i = 0; i < dx; i++)
+		{
+			if (d >= 0)
+			{
+				y += incy;
+				d += incNE;
+			}
+			else
+				d += incE;
+			x += incx;
+			points.push_back(x);
+			points.push_back(y);
+			if (is_vector)
+			{
+				points.push_back(dx);
+				points.push_back(dy);
+			}
+			else
+			{
+				points.push_back(c.x);
+				points.push_back(c.y);
+			}
+		}
+	}
+	else
+	{
+		points.push_back(x);
+		points.push_back(y);
+		if (is_vector)
+		{
+			points.push_back(dx);
+			points.push_back(dy);
+		}
+		else
+		{
+			points.push_back(c.x);
+			points.push_back(c.y);
+		}
+		d = 2 * dx - dy;
+		incNE = 2 * (dx - dy);
+		incE = 2 * dx;
+		for (i = 0; i < dy; i++)
+		{
+			if (d >= 0)
+			{
+				x += incx;
+				d += incNE;
+			}
+			else
+				d += incE;
+			y += incy;
+			points.push_back(x);
+			points.push_back(y);
+			if (is_vector)
+			{
+				points.push_back(dx);
+				points.push_back(dy);
+			}
+			else
+			{
+				points.push_back(c.x);
+				points.push_back(c.y);
+			}
+		}
+	}
+	return { dx, dy };
 }
 
 ///
 /// Bresenham's circle drawing algorithm
 ///
-void makeCircle(std::vector<float>& points, int xc, int yc, int r)
+void makeCircle(std::vector<float>& points, int xc, int yc, int r, Color c)
 {
     int x = 0;
     int y = r;
@@ -180,27 +227,43 @@ void makeCircle(std::vector<float>& points, int xc, int yc, int r)
     int deltaSE = -2 * r + 5;
     points.push_back(x + xc);
     points.push_back(y + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(-x + xc);
     points.push_back(y + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(x + xc);
     points.push_back(-y + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(-x + xc);
     points.push_back(-y + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(y + xc);
     points.push_back(x + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(-y + xc);
     points.push_back(x + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(y + xc);
     points.push_back(-x + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     points.push_back(-y + xc);
     points.push_back(-x + yc);
+    points.push_back(c.x);
+    points.push_back(c.y);
 
     while (y > x)
     {
@@ -220,27 +283,43 @@ void makeCircle(std::vector<float>& points, int xc, int yc, int r)
         x = x + 1;
         points.push_back(x + xc);
         points.push_back(y + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(-x + xc);
         points.push_back(y + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(x + xc);
         points.push_back(-y + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(-x + xc);
         points.push_back(-y + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(y + xc);
         points.push_back(x + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(-y + xc);
         points.push_back(x + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(y + xc);
         points.push_back(-x + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
 
         points.push_back(-y + xc);
         points.push_back(-x + yc);
+        points.push_back(c.x);
+        points.push_back(c.y);
     }
 }
 
@@ -259,25 +338,25 @@ float invSqrt(float x) {
 ///
 /// Makes an arrow head by calculating the 3 points and calling makeLine() on each pair
 ///
-void makeArrowHead(std::vector<float>& points, int x1, int y1, Vec vector)
+
+void makeArrowHead(std::vector<float>& points, int x1, int y1, Vec vector, Color c)
 {
     float normalization_factor = invSqrt(vector.i * vector.i + vector.j * vector.j);
-    int normalized_i = 5 * vector.i * normalization_factor;
-    int normalized_j = 5 *vector.j * normalization_factor;
-    makeLine(points, x1 + vector.i, y1 + vector.j, x1 + vector.i - normalized_j - normalized_i, y1 + vector.j + normalized_i - normalized_j);
-    makeLine(points, x1 + vector.i, y1 + vector.j, x1 + vector.i - normalized_i + normalized_j, y1 + vector.j - normalized_i - normalized_j);
-    makeLine(points, x1 + vector.i - normalized_j - normalized_i, y1 + vector.j + normalized_i - normalized_j, x1 + vector.i - normalized_i + normalized_j, y1 + vector.j - normalized_i - normalized_j);
+    float normalized_i = 5 * vector.i * normalization_factor;
+    float normalized_j = 5 * vector.j * normalization_factor;
+    makeLine(points, x1 + vector.i, y1 + vector.j, x1 + vector.i - normalized_j - normalized_i, y1 + vector.j + normalized_i - normalized_j, false, c);
+    makeLine(points, x1 + vector.i, y1 + vector.j, x1 + vector.i - normalized_i + normalized_j, y1 + vector.j - normalized_i - normalized_j, false, c);
+    makeLine(points, x1 + vector.i - normalized_j - normalized_i, y1 + vector.j + normalized_i - normalized_j, x1 + vector.i - normalized_i + normalized_j, y1 + vector.j - normalized_i - normalized_j, false, c);
 }
-
 ///
 /// The vector field function
 ///
-Vec vectorField(int x, int y)
+Vec vectorField(float x, float y)
 {
-    return { x*x+y*y, x*x-y*y};
+    return { x*x*y+y*x*x, x*x*y-y*y*x };
 }
 
-float scale = 0.001;
+float scale = 0.000001;
 float sparsity = 50;
 float zoom = 1;
 bool drawn = false;
@@ -289,15 +368,11 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     if (yoffset > 0)
     {
-        std::cout << zoom << std::endl;
-        std::cout << sparsity << std::endl;
         zoom *= 1.1;
         sparsity *= 1.1;
     }
     else if (yoffset < 0)
     {
-        std::cout << zoom << std::endl;
-        std::cout << sparsity << std::endl;
         zoom /= 1.1;
         sparsity /= 1.1;
     }
@@ -309,7 +384,7 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 ///
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-    if (sparsity>5 && button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    if (sparsity > 5 && button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
         sparsity *= 1.1;
     else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
         sparsity /= 1.1;
@@ -322,7 +397,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_S && action == GLFW_PRESS)
-        sparsity=50;
+        sparsity = 50;
     else if (key == GLFW_KEY_Z && action == GLFW_PRESS)
         zoom = 1;
     drawn = false;
@@ -358,7 +433,7 @@ int main(void)
     std::vector<float> points;
 
     Vec vector;
-    
+
     float* points_array;         //this is for passing the vector as a pointer to the buffer data. Eventually this will be set to &points[0]
 
     unsigned int buffer;
@@ -366,7 +441,10 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (const void*)(sizeof(float) * 2));
 
     ShaderProgramSource source = parseShader("res/shaders/basic.shader");
     unsigned int shader = createShader(source.vertex_source, source.fragment_source);
@@ -376,25 +454,31 @@ int main(void)
     glfwSetMouseButtonCallback(window, mouse_button_callback);
     glfwSetKeyCallback(window, key_callback);
 
+    double tpp;
+    double sum_tpp=0;
+    int n=0;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
         if (!drawn)
         {
+            n++;
             points.clear();
             //The drawwing starts:
+            std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
+            Color c;
             for (int i = -500; i < 501; i = i + sparsity)
             {
                 for (int j = -500; j < 501; j = j + sparsity)
                 {
                     vector = vectorField(i * 1 / zoom, j * 1 / zoom);     //If zoom is 2, then we have to find the vector not at (i,j) but at (i/2,j/2) and then plot it at (i,j)
-                    //           makeCircle(points, i, j, sqrt(vector.i*vector.i/100+vector.j*vector.j/100));
-                    makeLine(points, i, j, i + vector.i * zoom * scale, j + vector.j * zoom * scale);     //We don't want to plot from (i,j) to (i+vector.i, j+vector.j) cause that would be massive hence the scale. Also as zoom increases, the vectors should appear bigger
-                    makeCircle(points, i, j, 2); //teeny tiny circle at base for aesthetic
-                    makeArrowHead(points, i, j, { (int)(vector.i * zoom * scale), (int)(vector.j * zoom * scale) }); //make an arrow head
+                    //           makeCircle(points, i, j, sqrt(vector.i * zoom * scale * vector.i * zoom * scale + vector.j * zoom * scale * vector.j * zoom * scale));
+                    c = makeLine(points, i, j, i + vector.i * zoom * scale, j + vector.j * zoom * scale, true, { 0,0 });     //We don't want to plot from (i,j) to (i+vector.i, j+vector.j) cause that would be massive hence the scale. Also as zoom increases, the vectors should appear bigger
+                    makeCircle(points, i, j, 2, c); //teeny tiny circle at base for aesthetic
+                    makeArrowHead(points, i, j, { (vector.i * zoom * scale), (vector.j * zoom * scale) }, c); //make an arrow head
                 }
             }
 
@@ -405,9 +489,17 @@ int main(void)
             glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(float), points_array, GL_STATIC_DRAW);
 
             drawn = true;
+            std::chrono::time_point<std::chrono::system_clock> end_time = std::chrono::system_clock::now();
+            std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+            std::cout << "Number of points: " << points.size() / 4 << std::endl;
+            std::cout << "Duration to compute: " << duration.count() << std::endl;
+            tpp = (double)duration.count() / (double)points.size();
+            sum_tpp += tpp;
+            std::cout << "Time per point: " << tpp << std::endl;
+            std::cout << "Average time per point so far: " << sum_tpp/(double)n << std::endl << std::endl;
         }
 
-        glDrawArrays(GL_POINTS, 0, points.size() / 2);
+        glDrawArrays(GL_POINTS, 0, points.size() / 4);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
